b1dd60bf40c9319cf0654804976da8398f01a591
Shantanu Gupta <Gupta.Shantanu6@gmail.com>
2021-03-21 18:02:19 +0530

    first

---
+++ src/pages/MemoriesPage.vue
@@ -0,0 +1,34 @@
+<template>
+    <ion-page>
+        <ion-header>
+            <ion-toolbar>
+                <ion-title>All Memories</ion-title>
+            </ion-toolbar>
+        </ion-header>
+        <ion-content>
+            <ion-list>
+                <ion-item>Shantanu</ion-item>
+                <ion-item>Shreya</ion-item>
+            </ion-list>
+        </ion-content>
+    </ion-page>
+</template>
+
+<script>
+import {IonPage, IonHeader, IonContent, IonTitle, IonToolbar, IonList, IonItem} from '@ionic/vue'
+export default {
+    components: {
+        IonTitle,
+        IonContent,
+        IonHeader,
+        IonPage,
+        IonToolbar,
+        IonList,
+        IonItem
+    },
+};
+</script>
+
+
+
+ 
